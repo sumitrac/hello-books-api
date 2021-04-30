610b9d3b648064c1ed5a10a2d0df15e0d7500f31
Sumitra <sumitrac2008@gmail.com>
2021-04-30 10:05:41 -0700

    updated

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,29 @@
 from flask import Blueprint
 
+hello_world_bp = Blueprint("hello_world", __name__)
+
+@hello_world_bp.route('/hello-world', methods=["Get"])
+def get_hello_world():
+    my_response = "Hello, World!"
+    return my_response
+
+@hello_world_bp.route('/hello-world/JSON', methods=["Get"])
+def hello_world_json():
+    return {
+        "name": "Sumi",
+        "message": "Hello",
+        "hobbies": ["Reading", "Coding", "Gardening"],
+    }, 2001
+
+
+@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
+def broken_endpoint():
+    response_body = {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+    new_hobby = "Surfing"
+    response_body["hobbies"].append(new_hobby)
+    return response_body
+
