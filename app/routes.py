2264f5d9e4cc4d8bdbcd3322f7f07ef8738dd4b0
Sumitra <sumitrac2008@gmail.com>
2021-05-01 22:27:22 -0700

    updated

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,50 @@
+from app import db
 from flask import Blueprint
+from flask import request
+from .models.book import Book 
 
-hello_world_bp = Blueprint("hello_world", __name__)
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+@books.bp.route("", methods=["POST"])
+def books():
+    request_body = request.get_json()
 
-@hello_world_bp.route('/hello-world', methods=["Get"])
-def get_hello_world():
-    my_response = "Hello, World!"
-    return my_response
+    new_book = Book(title = request_body["title"],
+                        describtion = request_body["description"])
 
-@hello_world_bp.route('/hello-world/JSON', methods=["Get"])
-def hello_world_json():
-    return {
-        "name": "Sumi",
-        "message": "Hello",
-        "hobbies": ["Reading", "Coding", "Gardening"],
-    }, 2001
+    db.session.add(new_book)
+    db.session.commit()
+
+    return (f"book #{new_book.title} has been created", 201)
 
 
-@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
-def broken_endpoint():
-    response_body = {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-    new_hobby = "Surfing"
-    response_body["hobbies"].append(new_hobby)
-    return response_body
+
+
+
+
+# hello_world_bp = Blueprint("hello_world", __name__)
+
+# @hello_world_bp.route('/hello-world', methods=["Get"])
+# def get_hello_world():
+#     my_response = "Hello, World!"
+#     return my_response
+
+# @hello_world_bp.route('/hello-world/JSON', methods=["Get"])
+# def hello_world_json():
+#     return {
+#         "name": "Sumi",
+#         "message": "Hello",
+#         "hobbies": ["Reading", "Coding", "Gardening"],
+#     }, 2001
+
+
+# @hello_world_bp.route("/broken-endpoint-with-broken-server-code")
+# def broken_endpoint():
+#     response_body = {
+#         "name": "Ada Lovelace",
+#         "message": "Hello!",
+#         "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+#     }
+#     new_hobby = "Surfing"
+#     response_body["hobbies"].append(new_hobby)
+#     return response_body
 
