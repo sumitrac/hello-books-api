4e5ef6f0cbccead273d94a2acb858c4133726530
Sumitra <sumitrac2008@gmail.com>
2021-05-04 03:06:07 -0700

    Created helper function to check if int, for book route- divided into two functions: GET and POST, created a helper function to return book model as dictionary for clean code

--- app/models/book.py
+++ app/models/book.py
@@ -5,6 +5,13 @@ class Book(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
 
+#function to return dictionary
+    def to_json(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description
+        }
 
     def to_string(self):
         return f"{self.id}: {self.title} Description: {self.description}"
--- app/routes.py
+++ app/routes.py
@@ -5,51 +5,72 @@ from flask import jsonify
 from .models.book import Book 
 
 books_bp = Blueprint("books", __name__, url_prefix="/book")
+
+#try and except error
+def is_int(value):
+    try:
+        return int(value)
+    except ValueError:
+        return False 
+
 @books_bp.route("/book_id>", methods=["GET"], strict_slashes=False)
 def get_single_book(book_id):
     #Try to find the book with the given id 
+    if not is_int(book_id):
+        return {
+            "message": f"ID {book_id} must be an integer",
+            "success": False
+        }, 400
+
     book = Book.query.get(book_id)
 
     if book:
-        return {
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        }, 200
+        # return {
+        #     "id": book.id,
+        #     "title": book.title,
+        #     "description": book.description
+        # }, 200
+        return book.to_json(), 200 #using a function for clean code 
+
     return {
         "message": f"Book with id {book_id} was not found",
         "success": False,
     }, 400
 
+@books.bp.route("", methods=["GET"], strict_slashes=False)
+def books_index():
+    books = Book.query.all()
+    books_response = {}
 
-@books.bp.route("", methods=["POST", "GET"], strict_slashes=False)
+    for book in books:
+        # books_response.append({
+        #     "id": book.id,
+        #     "title": book.title,
+        #     "description": book.description
+        # })
+        books_response.append(book.to_json()) #using function for clean code 
+    return jsonify(books_response), 200
+
+
+@books.bp.route("", methods=["POST"], strict_slashes=False)
 
 def books():
-    if request.method == "GET":
-        books = Book.query.all()
-        books_response = {}
+    request_body = request.get_json()
 
-        for book in books:
-            books_response.append({
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-                })
-        return jsonify(books_response), 200
+    new_book = Book(title = request_body["title"],
+                description=request_body["description"])
 
-    else:
-        request_body = request.get_json()
+    db.session.add(new_book)
+    db.session.commit()
+    # return (f"book {new_book.title} has been created", 201)
+    return {
+        "success": True,
+        "message": f"Book {new_book.title} has been created"
+    }, 201
 
-        new_book = Book(title = request_body["title"],
-                            description=request_body["description"])
 
-        db.session.add(new_book)
-        db.session.commit()
-        # return (f"book {new_book.title} has been created", 201)
-        return {
-            "success": True,
-            "message": f"Book {new_book.title} has been created"
-        }, 201
+
+
 
 
 # hello_world_bp = Blueprint("hello_world", __name__)
