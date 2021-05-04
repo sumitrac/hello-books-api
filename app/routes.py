5d79c3c1bee4db0d1c9bfa6595036e43d7676a06
Sumitra <sumitrac2008@gmail.com>
2021-05-04 01:54:24 -0700

    import of book_models, book_bp

--- app/routes.py
+++ app/routes.py
@@ -3,22 +3,23 @@ from flask import Blueprint
 from flask import request
 from .models.book import Book 
 
-books_bp = Blueprint("books", __name__, url_prefix="/books")
-@books.bp.route("", methods=["POST"])
+books_bp = Blueprint("books", __name__, url_prefix="/book")
+@books.bp.route("", methods=["POST"], strict_slashes=False)
+
 def books():
     request_body = request.get_json()
 
     new_book = Book(title = request_body["title"],
-                        describtion = request_body["description"])
+                        description=request_body["description"])
 
     db.session.add(new_book)
     db.session.commit()
 
-    return (f"book #{new_book.title} has been created", 201)
-
-
-
-
+    # return (f"book {new_book.title} has been created", 201)
+    return {
+        "success": True,
+        "message": f"Book {new_book.title} has been created"
+    }, 201
 
 
 # hello_world_bp = Blueprint("hello_world", __name__)
